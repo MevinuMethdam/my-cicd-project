3ee675b05660415e4d48086afe93c49c729b6554
Mevinu Methdam <mevinumethdam@yahoo.com>
2026-06-25 10:10:53 +0530

    Update to V2.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello System Engineer! My CI/CD Pipeline is working V1.0');
+    res.send('Hello System Engineer! My CI/CD Pipeline is working V2.0 - Zero Downtime Magic!');
 });
 
 app.listen(PORT, () => {
